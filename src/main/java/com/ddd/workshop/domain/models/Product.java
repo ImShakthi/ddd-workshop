4c61a78cc872adc5daa60128123d8e648f0ce311
Balasubramanian N <nsbala@thoughtworks.com>
2021-04-23 16:49:24 +0530

    Refactoring. Added additional tests.

--- src/main/java/com/ddd/workshop/domain/models/Product.java
+++ src/main/java/com/ddd/workshop/domain/models/Product.java
@@ -1,8 +1,12 @@
 package com.ddd.workshop.domain.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
+@AllArgsConstructor
 public class Product {
     private final String name;
+
 }
--- src/test/java/com/ddd/workshop/domain/models/CartTest.java
+++ src/test/java/com/ddd/workshop/domain/models/CartTest.java
@@ -1,6 +1,7 @@
 package com.ddd.workshop.domain.models;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
 
 public class CartTest extends TestCase {
     public void testShouldAddIpadProToCart() {
@@ -8,5 +9,14 @@ public class CartTest extends TestCase {
 
         Product product = new Product("IPad Pro");
         cart.add(product);
+        Assert.assertEquals("Ipad Pro", cart.getItems().get(0).getName());
+    }
+
+    public void testShouldAddHeroInkPenToCart() {
+        Cart cart = new Cart();
+        Product product = new Product("Hero Ink Pen");
+        cart.add(product);
+        Assert.assertEquals("Hero Ink Pen", cart.getItems().get(0).getName());
+
     }
 }
